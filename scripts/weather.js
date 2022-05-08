8ddd7486923abad0a3eb1c40881660cb863c9a96
ANTOND <71350868+antond15@users.noreply.github.com>
2022-05-08 22:13:38 +0200

    feat(settings): Add unit system selection

--- scripts/weather.js
+++ scripts/weather.js
@@ -2,14 +2,18 @@ let API_KEY, API_URL;
 
 const weatherElements = {
 	icon: document.getElementById('icon'),
-	temp: document.getElementById('temperature'),
-	desc: document.getElementById('description')
+	temp: document.getElementById('degrees'),
+	desc: document.getElementById('description'),
+	unit: document.getElementById('unit')
 }
 
 window.wallpaperPropertyListener = {
     applyUserProperties: function(properties) {
+		const unit = properties.units.value || 'metric';
+		weatherElements.unit.textContent = unit === 'metric' ? 'C' : 'F';
+
 		API_KEY = properties.key.value;
-		API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${properties.location.value || 'London'}&appid=${API_KEY}&units=metric&lang=${properties.language.value || 'en'}`
+		API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${properties.location.value || 'London'}&appid=${API_KEY}&units=${unit}&lang=${properties.language.value || 'en'}`;
 		updateWeather();
     }
 };
@@ -18,7 +22,7 @@ window.wallpaperPropertyListener = {
 
 const setWarning = () => {
 	weatherElements.icon.src = './visuals/warning.png';
-	weatherElements.temp.textContent = '401';
+	weatherElements.temp.textContent = '?';
 	weatherElements.desc.textContent = 'Invalid API key';
 }
 
@@ -44,7 +48,7 @@ const updateWeather = () => {
 		const { main, weather } = data;
 
 		weatherElements.icon.src = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
-		weatherElements.temp.textContent = `${Math.round(main.temp)} °C`;
+		weatherElements.temp.textContent = Math.round(main.temp);
 		weatherElements.desc.textContent = weather[0].description;
 	})
 }
